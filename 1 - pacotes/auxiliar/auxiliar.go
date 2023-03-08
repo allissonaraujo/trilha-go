155ac36f56bd357b372e7fe9e9921358b7b3a2e6
Allisson Rafael de Brito Araujo <allissonads@outlook.com>
2023-03-08 00:40:10 -0300

    Feat: Pacotes

---
+++ 1 - pacotes/auxiliar/auxiliar.go
@@ -0,0 +1,11 @@
+package auxiliar
+
+import (
+	"fmt"
+)
+
+// Função Escrever que escreve na tela
+func Escrever() {
+	fmt.Println("Escrevendo com o pacote auxiliar")
+	escrever2()
+}
---
+++ 1 - pacotes/auxiliar/auxiliar2.go
@@ -0,0 +1,8 @@
+package auxiliar
+
+import "fmt"
+
+//Função escrever2 que escreve na tela
+func escrever2() {
+	fmt.Println("Escrevendo com o pacote auxiliar 2 que só tem visibilidade entre os mesmos pacotes.")
+}
